517329dcea67e1cfed0d44b33d7b682a05aeecac
Jack Cullen <jpcullen20@gmail.com>
2020-05-29 10:38:34 -0600

    Add most popular ingredients method to chef model

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -8,4 +8,13 @@ class Chef <ApplicationRecord
               .distinct
   end
 
+  def most_popluar_ingredients
+    Ingredient.joins(:dishes)
+              .where("dishes.chef_id = ?", id)
+              .select("count(ingredients.id) AS times_used", "name")
+              .group("ingredients.id")
+              .order("times_used desc")
+              .limit(3)
+  end
+
 end
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -23,5 +23,31 @@ RSpec.describe Chef, type: :model do
 
       expect(chef.ingredients).to eq([ingredient1, ingredient2, ingredient3])
     end
+    it "most_popluar_ingredients" do
+      chef = Chef.create(name: "Chef")
+      dish = chef.dishes.create(name: "Dish", description: "Tasty")
+      dish2 = chef.dishes.create(name: "Dish", description: "Tasty")
+      dish3 = chef.dishes.create(name: "Dish", description: "Tasty")
+      dish4 = chef.dishes.create(name: "Dish", description: "Tasty")
+      ingredient1 = Ingredient.create(name: "Rice", calories: 10)
+      ingredient2 = Ingredient.create(name: "Eggs", calories: 100)
+      ingredient3 = Ingredient.create(name: "Potatoes", calories: 300)
+      ingredient4 = Ingredient.create(name: "Cereal", calories: 600)
+      ingredient5 = Ingredient.create(name: "FAKE", calories: 700)
+      DishIngredient.create(dish_id: dish.id, ingredient_id: ingredient1.id)
+      DishIngredient.create(dish_id: dish2.id, ingredient_id: ingredient2.id)
+      DishIngredient.create(dish_id: dish3.id, ingredient_id: ingredient2.id)
+      DishIngredient.create(dish_id: dish.id, ingredient_id: ingredient2.id)
+      DishIngredient.create(dish_id: dish.id, ingredient_id: ingredient3.id)
+      DishIngredient.create(dish_id: dish2.id, ingredient_id: ingredient3.id)
+      DishIngredient.create(dish_id: dish.id, ingredient_id: ingredient4.id)
+      DishIngredient.create(dish_id: dish2.id, ingredient_id: ingredient4.id)
+      DishIngredient.create(dish_id: dish3.id, ingredient_id: ingredient4.id)
+      DishIngredient.create(dish_id: dish4.id, ingredient_id: ingredient4.id)
+
+      expect(chef.most_popluar_ingredients.first.name).to eq("Cereal")
+      expect(chef.most_popluar_ingredients[1].name).to eq("Eggs")
+      expect(chef.most_popluar_ingredients.last.name).to eq("Potatoes")
   end
+end
 end
