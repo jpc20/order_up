2c75bcd81f51196ee3cd2f993461a6b1572879b8
Jack Cullen <jpcullen20@gmail.com>
2020-05-29 09:38:48 -0600

    Add total calories method to dish - finish story 2

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,5 +10,20 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through (:dish_ingredients)}
   end
-  
+  describe "methods" do
+    it "total_calories" do
+      chef = Chef.create(name: "Chef")
+      dish = chef.dishes.create(name: "Dish", description: "Tasty")
+      ingredient1 = Ingredient.create(name: "Rice", calories: 10)
+      ingredient2 = Ingredient.create(name: "Eggs", calories: 100)
+      ingredient3 = Ingredient.create(name: "Potatoes", calories: 300)
+      ingredient4 = Ingredient.create(name: "FAKE", calories: 600)
+      ingredient5 = Ingredient.create(name: "FAKE", calories: 700)
+      DishIngredient.create(dish_id: dish.id, ingredient_id: ingredient1.id)
+      DishIngredient.create(dish_id: dish.id, ingredient_id: ingredient2.id)
+      DishIngredient.create(dish_id: dish.id, ingredient_id: ingredient3.id)
+      expect(dish.total_calories).to eq(410)
+    end
+  end
+
 end
