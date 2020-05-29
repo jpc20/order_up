2a7ac3699ae8ca03c8bf1a1c5a474e2aab42ca64
Jack Cullen <jpcullen20@gmail.com>
2020-05-29 10:57:22 -0600

    Add most popular ingredients to the chefs show page - finish extension

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -14,9 +14,8 @@ RSpec.describe "Chef Show page" do
     @ingredient2 = Ingredient.create(name: "Eggs", calories: 100)
     @ingredient3 = Ingredient.create(name: "Potatoes", calories: 300)
     @ingredient4 = Ingredient.create(name: "Cereal", calories: 600)
-    @ingredient5 = Ingredient.create(name: "Milk", calories: 80)
-    @ingredient6 = Ingredient.create(name: "Raisins", calories: 7)
-    @ingredient7 = Ingredient.create(name: "Butter", calories: 300)
+    @ingredient5 = Ingredient.create(name: "Milk", calories: 600)
+
     DishIngredient.create(dish_id: @dish.id, ingredient_id: @ingredient1.id)
     DishIngredient.create(dish_id: @dish.id, ingredient_id: @ingredient2.id)
     DishIngredient.create(dish_id: @dish.id, ingredient_id: @ingredient3.id)
@@ -35,7 +34,6 @@ RSpec.describe "Chef Show page" do
     DishIngredient.create(dish_id: @dish4.id, ingredient_id: @ingredient1.id)
     DishIngredient.create(dish_id: @dish2.id, ingredient_id: @ingredient3.id)
     DishIngredient.create(dish_id: @dish3.id, ingredient_id: @ingredient3.id)
-    DishIngredient.create(dish_id: @dish.id, ingredient_id: @ingredient5.id)
   end
 
   it "visitor can go to chefs ingredient index from the show page" do
@@ -56,10 +54,7 @@ RSpec.describe "Chef Show page" do
       expect("#{@ingredient4.name}").to appear_before("#{@ingredient2.name}")
       expect("#{@ingredient2.name}").to appear_before("#{@ingredient1.name}")
 
-      expect(page).to_not have_content(@ingredient5.name)
       expect(page).to_not have_content(@ingredient3.name)
-      expect(page).to_not have_content(@ingredient6.name)
-      expect(page).to_not have_content(@ingredient7.name)
     end
   end
 end
