63d1d5da42dc1b37a5b251a6763bc2f62f5da054
Jack Cullen <jpcullen20@gmail.com>
2020-05-29 09:47:28 -0600

    Add spec for story 3

---
+++ spec/features/chefs/show_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "Chef Show page" do
+
+  before :each do
+    @chef = Chef.create(name: "Chef")
+    @dish = @chef.dishes.create(name: "Dish", description: "Tasty")
+    @dish2 = @chef.dishes.create(name: "Dish", description: "Tasty")
+    @ingredient1 = Ingredient.create(name: "Rice", calories: 10)
+    @ingredient2 = Ingredient.create(name: "Eggs", calories: 100)
+    @ingredient3 = Ingredient.create(name: "Potatoes", calories: 300)
+    @ingredient4 = Ingredient.create(name: "Cereal", calories: 600)
+    @ingredient5 = Ingredient.create(name: "FAKE", calories: 700)
+    DishIngredient.create(dish_id: @dish.id, ingredient_id: @ingredient1.id)
+    DishIngredient.create(dish_id: @dish.id, ingredient_id: @ingredient2.id)
+    DishIngredient.create(dish_id: @dish.id, ingredient_id: @ingredient3.id)
+  end
+
+  it "text" do
+    visit "/chefs/#{@chef.id}"
+    expect(page).to have_content(@chef.name)
+    click_link("Ingredients used by #{@chef.name}")
+    expect(current_path).to eq("/chefs/#{@chef.id}/ingredients")
+    expect(page).to have_content(@ingredient1.name)
+    expect(page).to have_content(@ingredient2.name)
+    expect(page).to have_content(@ingredient3.name)
+    expect(page).to have_content(@ingredient4.name)
+    expect(page).to_not have_content(@ingredient5.name)
+  end
+end
